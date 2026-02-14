0d237740d7f3c890900fce625c3242e6f86e5931
Travis Smith <tmsmith@travismsmith.com>
2026-02-14 17:13:48 -0500

    Changed to sh compatible read

--- .chezmoiscripts/darwin/run_once_before_install_1password.sh
+++ .chezmoiscripts/darwin/run_once_before_install_1password.sh
@@ -3,4 +3,5 @@
 brew install 1password 1password-cli
 
 echo "1Password and 1Password CLI installed. Please run launch the 1Password app and sign in to your account before proceeding with the rest of the setup."
-read -p "Press any key to continue after signing in to 1Password..."
+echo "Press any key to continue after signing in to 1Password..."
+read response
