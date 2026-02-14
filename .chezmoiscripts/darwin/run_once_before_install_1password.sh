1cc26dfdb23edbcc062fe8d950d12e890230b679
Travis Smith <tmsmith@travismsmith.com>
2026-02-14 17:06:24 -0500

    Added pause

--- .chezmoiscripts/darwin/run_once_before_install_1password.sh
+++ .chezmoiscripts/darwin/run_once_before_install_1password.sh
@@ -1,3 +1,6 @@
 #!/bin/zsh
 
 brew install 1password 1password-cli
+
+echo "1Password and 1Password CLI installed. Please run launch the 1Password app and sign in to your account before proceeding with the rest of the setup."
+pause "Press any key to continue after signing in to 1Password..."
