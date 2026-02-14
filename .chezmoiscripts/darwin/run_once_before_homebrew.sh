407c89d9ecfcc6b1d6962eef35ba6bc88b5f9965
Travis Smith <tmsmith@travismsmith.com>
2026-02-14 16:01:36 -0500

    Updated homebrew install

--- .chezmoiscripts/darwin/run_once_before_homebrew.sh
+++ .chezmoiscripts/darwin/run_once_before_homebrew.sh
@@ -6,9 +6,4 @@ if command -v brew &>/dev/null; then
     echo 'Homebrew is already installed'
 else
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    (
-        echo
-        echo 'eval "$(/opt/homebrew/bin/brew shellenv)"'
-    ) >>$HOME/.zprofile
-    eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
