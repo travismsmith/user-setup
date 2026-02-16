e3303134949657e30c09c83e8e78d9de0c61260d
Travis Smith <tmsmith@travismsmith.com>
2026-02-16 11:08:44 -0500

    Added new way to install xcode clt

--- .chezmoiscripts/darwin/run_once_before_homebrew.sh
+++ .chezmoiscripts/darwin/run_once_before_homebrew.sh
@@ -1,6 +1,16 @@
-#!/bin/sh
+#!/bin/zsh
 
-xcode-select --install > /dev/null 2>&1 || echo "XCode already installed"
+echo "Checking Command Line Tools for Xcode"
+xcode-select -p &> /dev/null
+if [ $? -ne 0 ]; then
+  echo "Command Line Tools for Xcode not found. Installing from softwareupdate…"
+# This temporary file prompts the 'softwareupdate' utility to list the Command Line Tools
+  touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+  PROD=$(softwareupdate -l | grep "\*.*Command Line" | tail -n 1 | sed 's/^[^C]* //')
+  softwareupdate -i "$PROD" --verbose;
+else
+  echo "Command Line Tools for Xcode have been installed."
+fi
 
 # Install Homebrew if necessary
 if command -v brew &>/dev/null; then
