721e16ae233a74001c8021588d5f35bf38603624
Travis Smith <tmsmith@travismsmith.com>
2026-02-16 17:11:19 -0500

    Removed unnecessary commands for homebrew

--- .chezmoiscripts/darwin/run_once_before_homebrew.sh
+++ .chezmoiscripts/darwin/run_once_before_homebrew.sh
@@ -17,7 +17,4 @@ if command -v brew &>/dev/null; then
     echo 'Homebrew is already installed'
 else
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    echo >> ~/.zprofile
-    echo 'eval "$(/opt/homebrew/bin/brew shellenv zsh)"' >> ~/.zprofile
-    eval "$(/opt/homebrew/bin/brew shellenv zsh)"
 fi
