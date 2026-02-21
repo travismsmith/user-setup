2e8b87cb861072655ab6859c0c70b70836a59123
Travis Smith <tmsmith@travismsmith.com>
2026-02-21 11:54:39 -0500

    Added iterm2 and FDA

--- .chezmoiscripts/darwin/run_after_confirmation.sh
+++ .chezmoiscripts/darwin/run_after_confirmation.sh
@@ -4,6 +4,7 @@ echo "--------------------------------------------------------------------------
 echo "| Dependencies have been installed. Please complete the following actions before proceeding:          |"
 echo "| - Launch the 1Password app and sign in to your account.                                             |"
 echo "| - Go to settings and click on 'Developer' in the sidebar, then enable Integrate with 1Password CLI. |"
+echo "| - Give iterm2 Full Disk Access                                                                      |"
 echo "| - Run \`gh auth login --web\` to authenticate the GitHub CLI with your account.                       |"
 echo "| - Run \`chezmoi init --apply <github-username>\` to apply the user's configuration files.             |"
 echo "-------------------------------------------------------------------------------------------------------"
